bd0ffb634e796178c2b3e774492ed880b6eabba8
danielzinchenko <38410644+danielzinchenko@users.noreply.github.com>
2018-05-03 19:41:59 +0300

    dfgsd
    bdhbdhbdfd

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
+<<<<<<< HEAD
+$(document).ready(function() {
+	$(".item").magnificPopup({
+		type : 'image',
+		gallery : {
+			enabled : true
+		},
+		removalDelay: 300,
+		mainClass: 'mfp-fade'
+	});
+});
+=======
 	$(function () {
 		$('.card').hover(function () {
 			$('.card-body').slideUp(8000);
 		});
 	});
+>>>>>>> 0d0c1c7385b22136a45612a92f007568ef21b45b
--- src/js/test/script.js
+++ src/js/test/script.js
@@ -1,7 +1,42 @@
+<<<<<<< HEAD
+$(document).ready(function() {
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+});
+
+=======
+>>>>>>> 0d0c1c7385b22136a45612a92f007568ef21b45b
 function fun1() {
 	var a = prompt(' 2+2=? ', '');	
 
 	if (a==4) {
+<<<<<<< HEAD
+	alert('Правильно! ');
+	}
+	 else if (a==null ) {
+	alert(' Операция отменена');
+	}
+	else if (a=='') {
+	alert(' Введите число ');
+	}
+	else  {
+	alert(' Не правильно ');
+
+}
+}
+
+=======
 	alert(' ��������� ');
 	}
 	 else if (a==null ) {
@@ -15,3 +50,4 @@ function fun1() {
 
 }
 }
+>>>>>>> 0d0c1c7385b22136a45612a92f007568ef21b45b
